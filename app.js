02ffe48839b8f10c1e62827a9f715039690725b5
GWartelle <115559211+GWartelle@users.noreply.github.com>
2024-03-08 22:31:48 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,32 +1,40 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const path = require('path');
+const express = require("express");
+const mongoose = require("mongoose");
+const path = require("path");
 
-const stuffRoutes = require('./routes/stuff');
-const userRoutes = require('./routes/user');
+const stuffRoutes = require("./routes/stuff");
+const userRoutes = require("./routes/user");
 
-mongoose.connect('mongodb+srv://gwartelle:ozszefJPZabNu2eG@cluster0.0ulrxvy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', 
-    { useNewUrlParser: true,
-      useUnifiedTopology: true })
-    .then(() => console.log('Connexion à MongoDB réussie !'))
-    .catch(() => console.log('Connexion à MongoDB échouée !'));
+mongoose
+  .connect(
+    "mongodb+srv://gwartelle:ozszefJPZabNu2eG@cluster0.0ulrxvy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
+  .then(() => console.log("Connexion à MongoDB réussie !"))
+  .catch(() => console.log("Connexion à MongoDB échouée !"));
 
 // Cette base de donnée sera supprimée suite à ce cours
 // Je vous invite à créer votre propre base de donnée.
-    
+
 const app = express();
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-    next();
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
 });
 
 app.use(express.json());
 
-app.use('/api/stuff', stuffRoutes);
-app.use('/api/auth', userRoutes);
-app.use('/images', express.static(path.join(__dirname, 'images')));
+app.use("/api/stuff", stuffRoutes);
+app.use("/api/auth", userRoutes);
+app.use("/images", express.static(path.join(__dirname, "images")));
 
 module.exports = app;
